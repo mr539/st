a6bf03097095b3ca63a114b7947b90f232b6dfd3
mr <martin7273@protonmail.com>
2022-07-07 17:48:01 +0200

    update

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ float alpha = 1.0;
 //static char *font = "Source Code Pro:pixelsize=12:antialias=true:autohint=true";
 //static char *font = "Hack:pixelsize=12:antialias=true:autohint=true";
 //static char *font = "fira code:pixelsize=12:antialias=true:autohint=true";
-static char *font = "Terminus:size=14:antialias=false:autohint=true";
+static char *font = "Terminus:size=12:antialias=false:autohint=true";
 static int borderpx = 2;
 
 /*
